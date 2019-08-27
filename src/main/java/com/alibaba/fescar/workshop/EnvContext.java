b7be443fbaaab6fba5929b0d64589155f0ee7ed6
jimin <slievrly@gmail.com>
2019-08-27 13:57:28 +0800

    update remote seata-server service  (#14)

--- src/main/java/com/alibaba/fescar/workshop/EnvContext.java
+++ src/main/java/com/alibaba/fescar/workshop/EnvContext.java
@@ -6,5 +6,5 @@ package com.alibaba.fescar.workshop;
  */
 public class EnvContext {
 
-    public static DBType dbType = DBType.ORACLE;
+    public static DBType dbType = DBType.MYSQL;
 }
--- src/main/java/com/alibaba/fescar/workshop/service/impl/StorageServiceImpl.java
+++ src/main/java/com/alibaba/fescar/workshop/service/impl/StorageServiceImpl.java
@@ -46,13 +46,113 @@ public class StorageServiceImpl implements StorageService {
     @Override
     public void deduct(String commodityCode, int count) {
         LOGGER.info("Storage Service Begin ... xid: " + RootContext.getXID());
-        LOGGER.info("Deducting inventory SQL: update storage_tbl set count = count - {} where commodity_code = {}",
+        LOGGER.info("Deducting inventory SQL: update STORAGE_TBL set count = count - {} where commodity_code = {}",
             count, commodityCode);
 
-        jdbcTemplate.update("update STORAGE_TBL set count = count - ? where commodity_code = ?",
+        jdbcTemplate.update("update storage_tbl set count = count - ? where commodity_code = ?",
             count, commodityCode);
         LOGGER.info("Storage Service End ... ");
 
+        //test update
+        /*
+        final List<ArrayList> params = new ArrayList<>();
+        for (int i = 1000; i < 1003; i++) {
+            ArrayList arrayList = new ArrayList();
+            arrayList.add(i);
+            arrayList.add("C"+i);
+            arrayList.add(100);
+            params.add(arrayList);
+        }
+
+        //update
+        //String sql="update STORAGE_TBL set commodity_code=?, count=? where id=? and count=?";
+
+        String sql="delete from STORAGE_TBL where id=? and count=?";
+
+        try {
+            int[] affRows = jdbcTemplate.batchUpdate(sql,
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        //update
+                        //ps.setString(1, params.get(i).get(1).toString());
+                        //ps.setInt(2, Integer.parseInt(params.get(i).get(2).toString())+100);
+                        //ps.setLong(3, Long.parseLong(params.get(i).get(0).toString()));
+                        //ps.setInt(4, Integer.parseInt(params.get(i).get(2).toString()));
+
+                        ps.setLong(1, Long.parseLong(params.get(i).get(0).toString()));
+                        ps.setInt(2, Integer.parseInt(params.get(i).get(2).toString()));
+
+
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return params.size();
+                    }
+                });
+            int affTotal = 0;
+            for (int i : affRows) {
+                affTotal += i;
+            }
+            Assert.isTrue(affTotal == params.size(), "xx");
+        }catch (Exception exx){
+            StringBuilder sb=new StringBuilder();
+        }
+        */
+
+        // test inc batch
+        /*
+        jdbcTemplate.update("insert into STORAGE_TBL(commodity_code,count) values('fake',999),('fake1',999),('fake2',
+        999)");
+        */
+
+        //jdbcTemplate.update("insert into STORAGE_TBL(commodity_code,count) values(?,?),(?,?),(?,?)",new
+        // Object[]{"fake1",999,"fake2",999,"fake3",999});
+
+        /* test no-inc batch
+        //jdbcTemplate.update("insert into STORAGE_TBL(id,commodity_code,count) values(111,'fake',999),(112,'fake1',
+        999),(113,'fake2',999)");
+
+        //jdbcTemplate.update("insert into STORAGE_TBL(id,commodity_code,count) values(?,?,?),(?,?,?),(?,?,?)",new
+        Object[]{111,"fake1",999,112,"fake2",999,113,"fake3",999});
+        */
+
+        //test batchUpdate
+        /*
+        final List<ArrayList> params = new ArrayList<>();
+        for (int i = 100; i < 200; i++) {
+            ArrayList arrayList = new ArrayList();
+            arrayList.add(i);
+            arrayList.add("batch");
+            arrayList.add(999);
+            params.add(arrayList);
+        }
+        try {
+            int[] affRows = jdbcTemplate.batchUpdate("insert into STORAGE_TBL(id,commodity_code,count) values(?,?,?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setLong(1, Long.parseLong(params.get(i).get(0).toString()));
+                        ps.setString(2, params.get(i).get(1).toString());
+                        ps.setInt(3, Integer.parseInt(params.get(i).get(2).toString()));
+
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return params.size();
+                    }
+                });
+            int affTotal = 0;
+            for (int i : affRows) {
+                affTotal += i;
+            }
+            Assert.isTrue(affTotal == params.size(), "xx");
+        }catch (Exception exx){
+            StringBuilder sb=new StringBuilder();
+        }
+        */
     }
 
 }
